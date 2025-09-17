f5a99e7f1015b5a32fc57b40a78518a2dc792aee
xenitsu889 <rajdeepsinghchavda7@gmail.com>
2025-09-17 15:18:11 +0530

    init

--- src/routes/bfl.ts
+++ src/routes/bfl.ts
@@ -1,5 +1,4 @@
 import { Router } from 'express';
-<<<<<<< HEAD
 import { bflController } from '../controllers/bflController';
 import { validateBflGenerate } from '../middlewares/validateBflGenerate';
 
@@ -8,10 +7,3 @@ const router = Router();
 router.post('/generate', validateBflGenerate, bflController.generate);
 
 export default router;
-=======
-const router = Router();
-router.get('/', (_req, res) => res.json({ ok: true }));
-export default router;
-
-
->>>>>>> 0865155ce1f1203295733682fae733abf57333b9
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,18 +1,8 @@
 import { Router } from 'express';
-<<<<<<< HEAD
-=======
-import generateRoutes from './generate';
-import fluxRoutes from './flux';
->>>>>>> 0865155ce1f1203295733682fae733abf57333b9
 import bflRoutes from './bfl';
 
 const router = Router();
 
-<<<<<<< HEAD
-=======
-router.use('/generate', generateRoutes);
-router.use('/flux', fluxRoutes);
->>>>>>> 0865155ce1f1203295733682fae733abf57333b9
 router.use('/bfl', bflRoutes);
 
 export default router;
