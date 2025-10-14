a4ac9e1f02ef3af8cbc68c3fff18c453554b32d5
xenitsu889 <rajdeepsinghchavda7@gmail.com>
2025-10-14 11:44:23 +0530

    kling models integrated

--- dist/routes/credits.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const authMiddleware_1 = require("../middlewares/authMiddleware");
-const creditsController_1 = require("../controllers/creditsController");
-const router = (0, express_1.Router)();
-router.get('/me', authMiddleware_1.requireAuth, creditsController_1.creditsController.me);
-exports.default = router;
