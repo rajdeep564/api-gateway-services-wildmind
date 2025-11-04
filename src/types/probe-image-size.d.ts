c3eb01f9635a129a5985101fa5effc33d84fc6cf
xenitsu889 <rajdeepsinghchavda7@gmail.com>
2025-11-04 18:17:06 +0530

    asdsad

--- src/types/probe-image-size.d.ts
+++ src/types/probe-image-size.d.ts
@@ -1,1 +1,12 @@
-declare module 'probe-image-size';
+declare module 'probe-image-size' {
+	export type ProbeResult = {
+		width: number;
+		height: number;
+		type?: string;
+		mime?: string;
+		length?: number;
+		url?: string;
+	};
+	const probe: (src: any) => Promise<ProbeResult>;
+	export default probe;
+}
